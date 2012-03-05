2c29597e429ff690ebce7d7e938118fc411fd232
Tara L Andrews <tla@mit.edu>
2012-03-05 15:50:08 +0100

    do quote conversion in all cases; use Unicode property attribute for match

--- lib/Morph/Perseus.pm
+++ lib/Morph/Perseus.pm
@@ -18,18 +18,23 @@ __PACKAGE__->load_namespaces;
 use feature 'say';
 use Encode qw/ decode_utf8 /;
 use Unicode::Normalize;
-# TODO use Unicode::UCD qw/ charinfo /;
 
 sub lookup {
 	my( $self, $word ) = @_;
 	$word = NFC( $word );
+	# Convert to curly quotes where possible
+	$word =~ s/^['\x{1fbd}]/\x{2018}/;
+	$word =~ s/['\x{1fbd}]$/\x{2019}/;
 
+	my $is_exact = 1;
 	my @results = $self->_straight_match( $word );
 	@results = $self->_lc_match( $word ) unless @results;
 	@results = $self->_name_match( $word ) unless @results;
-	@results = $self->_quote_match( $word ) unless @results;
-	@results = $self->_accentless_match( $word ) unless @results;
-	return @results;
+	unless( @results ) {
+		$is_exact = 0;
+		@results = $self->_accentless_match( $word );
+	}
+	return( { 'exact_match' => $is_exact, 'objects' => \@results } );
 }
 
 sub _straight_match {
@@ -50,14 +55,6 @@ sub _name_match {
 	return $rs->all;
 }
 
-sub _quote_match {
-	my( $self, $word ) = @_;
-	$word =~ s/^['\x{1fbd}]/\x{2018}/;
-	$word =~ s/['\x{1fbd}]$/\x{2019}/;
-	my $rs = $self->resultset('Lexicon')->search({ 'token' => $word });
-	return $rs->all;
-}
-
 sub _accentless_match {
 	my( $self, $word ) = @_;
 	my @chars = split( '', $word );
@@ -70,7 +67,7 @@ sub _accentless_match {
 		} else {
 			push( @token, '_' );
 			foreach my $dc ( split( '', NFD( $c ) ) ) {
-				push( @matchstr, $dc ) if $dc =~ /[[:alpha:]]/;
+				push( @matchstr, $dc ) if $dc =~ /\pL/;
 			}
 		}
 	}
@@ -84,7 +81,7 @@ sub _accentless_match {
 			my @rmatch;
 			foreach my $c ( @rchars ) {
 				foreach my $dc ( split( '', NFD( $c ) ) ) {
-					push( @rmatch, $dc ) if $dc =~ /[[:alpha:]]/;
+					push( @rmatch, $dc ) if $dc =~ /\pL/;
 				}
 			}
 			push( @resultset, $match ) if join( '', @matchstr ) eq join( '', @rmatch );
